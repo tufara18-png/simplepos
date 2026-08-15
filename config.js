5e009346e3dae5d9f873208184ff17a403a6aac0
tufara18-png <tufara18@gmail.com>
2026-08-15 14:45:00 -0400

    Add runtime config template

---
+++ config.js
@@ -0,0 +1,11 @@
+window.SIMPLEPOS_CONFIG = {
+  // Supabase is optional until a project is created.
+  supabaseUrl: '',
+  supabasePublishableKey: '',
+  restaurantId: '',
+
+  // The PWA is served by the local print bridge in the simplest deployment.
+  // Printing then only needs the printer IPs below.
+  kitchenPrinterIp: '',
+  receiptPrinterIp: ''
+};
